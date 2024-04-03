b060a6268cd9adb36697c57820951d25cf8b87f4
Your Name <you@example.com>
2024-04-03 08:41:03 -0700

    Push my code to github

---
+++ index.js
@@ -0,0 +1,19 @@
+//Logical statment
+var num1 = 10;
+var num2 = 20;
+if (num1 === num2) {
+    console.log("Both are equal");
+}
+else if (num1 < num2) {
+    console.log("Yes num1 is less than to num2 10 < 20");
+}
+else {
+    console.log("They both are differnt ");
+}
+var isRaining = true;
+if (isRaining) {
+    console.log("wear a raincoat");
+}
+else {
+    console.log("Wear a sungalsses");
+}
---
+++ index.ts
@@ -0,0 +1,25 @@
+//Logical statment
+
+let num1 = 10;
+let num2 = 20;
+
+if(num1 === num2) {
+    console.log ("Both are equal");
+}
+
+else if (num1 < num2){
+    console.log("Yes num1 is less than to num2 10 < 20")
+}
+
+else {
+    console.log("They both are differnt ")
+}
+
+
+let isRaining: Boolean = true;
+let notRaining: boolean = false;
+if (isRaining=== notRaining){
+    console.log("wear a raincoat");
+} else {
+    console.log("Wear a sungalsses")
+}
